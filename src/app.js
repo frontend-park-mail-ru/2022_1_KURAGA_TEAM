0d139bcd65eb98de0eca9800a7c7002458598d06
Danya <danyat02@yandex.ru>
2022-03-06 17:01:33 +0300

    Изменил пару косячков

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 'use script'
 
-import {Router} from "./routing/Router.js";
+import {Router} from "./routing/router.js";
 import {RegViewClass} from "./views/regView/regViewClass.js";
 import {LoginViewClass} from "./views/loginView/loginViewClass.js";
 import {HomeViewClass} from "./views/homeView/homeViewClass.js";
