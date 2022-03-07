dfcec16b666de0998d389fb6bf92eeb669b7f632
Danya <danyat02@yandex.ru>
2022-03-07 21:47:15 +0300

    Исправил

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(morgan('dev'));
 app.use(express.static(path.resolve(__dirname, '..', '.')));
 app.use(fallback('index.html', { root: (path.resolve(__dirname, '..', '.')) }));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Server listening port ${port}`);
