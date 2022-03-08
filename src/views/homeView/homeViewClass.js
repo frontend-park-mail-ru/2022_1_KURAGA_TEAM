605adbedf66d05820479d8b674155f2221c72d80
iiivan-lemon <lipton2807@yandex.ru>
2022-03-08 23:57:39 +0300

    change url

--- src/views/homeView/homeViewClass.js
+++ src/views/homeView/homeViewClass.js
@@ -20,7 +20,7 @@ export default class HomeViewClass {
                 }
 
                 data.then((data) => {
-                    const header = new HeaderClass("data.username");
+                    const header = new HeaderClass(data.username);
                     const mainMovie = new MainMovieClass();
                     const carouselPop = new carousel("Pop");
                     const carouselTop = new carousel("Top");
