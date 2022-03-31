48746807c87b07e8c60082d3a43f2272c02f83fe
iiivan-lemon <lipton2807@yandex.ru>
2022-03-31 18:41:28 +0300

    visibility carousel buttons

--- src/components/carousel/movingCarousel.js
+++ src/components/carousel/movingCarousel.js
@@ -1,8 +1,6 @@
 export default function MovingCarousel(setting) {
 
 
-
-
     if (document.querySelector(setting.wrap) === null) {
         console.error(`Carousel not fount selector ${setting.wrap}`);
         return;
@@ -11,20 +9,29 @@ export default function MovingCarousel(setting) {
     const privates = {};
 
     this.prev_slide = () => {
+        if (privates.opt.position - 1 <= 0) {
+            privates.sel.prev.style.visibility = `hidden`;
+        } else {
+            privates.sel.prev.style.visibility = `visible`;
+        }
+
         privates.opt.position--;
-
-
-
+        privates.sel.next.style.visibility = `visible`;
         privates.sel.wrap.style.transform = `translateX(-${privates.opt.position}00%)`;
     };
 
     this.next_slide = () => {
         privates.opt.position++;
+        if (privates.opt.position + 1 >= privates.opt.max_position) {
+            privates.sel.next.style.visibility = `hidden`;
+        } else {
+            privates.sel.next.style.visibility = `visible`;
+        }
 
         if (privates.opt.position >= privates.opt.max_position) {
             --privates.opt.position;
         }
-
+        privates.sel.prev.style.visibility = `visible`;
         privates.sel.wrap.style.transform = `translateX(-${privates.opt.position}00%)`;
     };
 
@@ -42,8 +49,9 @@ export default function MovingCarousel(setting) {
         position: 0,
         max_position: document.querySelector(privates.setting.wrap).children.length,
     };
-
-
+    if (privates.opt.max_position > 1) {
+        privates.sel.next.style.visibility = `visible`;
+    }
 
     if (privates.sel.prev !== null) {
         privates.sel.prev.addEventListener('click', () => {
@@ -52,6 +60,8 @@ export default function MovingCarousel(setting) {
     }
 
     if (privates.sel.next !== null) {
+
+
         privates.sel.next.addEventListener('click', () => {
             this.next_slide();
         });
