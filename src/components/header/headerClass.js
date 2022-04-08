23a791583709bad9ea88c5aa160c9d8bfa7f3cb3
iiivan-lemon <lipton2807@yandex.ru>
2022-04-08 13:33:57 +0300

    header corrections

--- src/components/header/headerClass.js
+++ src/components/header/headerClass.js
@@ -44,10 +44,12 @@ export default class HeaderClass {
                 verticalNavbar.classList.add('menuSymbol__action');
             }
         });
-        const profileIcon = document.querySelector('.profile-block');
+        const profileIcon = document.querySelector('.btn-profile');
         profileIcon.addEventListener('touchstart', (e) => {
             e.preventDefault();
             const profileMenu = document.querySelector(".dropdown-content");
+
+
             if (profileMenu.style.display === 'block') {
                 profileMenu.style.display = 'none';
             } else {
