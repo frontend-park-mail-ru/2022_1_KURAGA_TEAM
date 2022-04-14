29d6dc2f3d4a18b071f7e416d825c2d5ab1a2218
iiivan-lemon <lipton2807@yandex.ru>
2022-04-14 14:00:52 +0300

    fix username: add regExp

--- src/components/inputs/utils/regExp/regExp.ts
+++ src/components/inputs/utils/regExp/regExp.ts
@@ -3,6 +3,6 @@ export const regExp = {
     containsLetters: /^.*[a-zA-Z]+.*$/,
     minimum8Chars: /^.{8,}$/,
     containsNumbers: /^.*[0-9]+.*$/,
-    checkUsername: /^(?!.*\.\.)(?!\.)(?!.*\.$)(?!\d+$)[a-zA-Z0-9А-Яа-я.]{5,10}$/,
+    checkUsername: /^(?!.*\.\.)(?!\.)(?!.*\.$)(?!\d+$)[a-zA-Z0-9А-Яа-я.]{5,30}$/,
 };
 
