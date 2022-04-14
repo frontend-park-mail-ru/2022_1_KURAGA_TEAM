46053811c4d379dee144699faa645fe8d3fb3dee
iiivan-lemon <lipton2807@yandex.ru>
2022-04-14 14:12:14 +0300

    fix email: edit regExp

--- src/components/inputs/utils/regExp/regExp.ts
+++ src/components/inputs/utils/regExp/regExp.ts
@@ -1,5 +1,5 @@
 export const regExp = {
-    checkEmail: /.+@.+\..+/i,
+    checkEmail: /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/i,
     containsLetters: /^.*[a-zA-Z]+.*$/,
     minimum8Chars: /^.{8,}$/,
     containsNumbers: /^.*[0-9]+.*$/,
